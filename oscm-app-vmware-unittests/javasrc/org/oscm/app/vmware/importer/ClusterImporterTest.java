624d30c7e76d2c51dbd8517b6fa6a249ebe10fcb
worf <worf@estworf.intern.est.fujitsu.com>
2019-02-01 16:04:51 +0100

    Add header to class

--- oscm-app-vmware-unittests/javasrc/org/oscm/app/vmware/importer/ClusterImporterTest.java
+++ oscm-app-vmware-unittests/javasrc/org/oscm/app/vmware/importer/ClusterImporterTest.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ *
+ *  Copyright FUJITSU LIMITED 2018
+ *
+ *  Creation Date: 2018-06-27
+ *
+ *******************************************************************************/
+
 package org.oscm.app.vmware.importer;
 
 import static org.junit.Assert.*;
