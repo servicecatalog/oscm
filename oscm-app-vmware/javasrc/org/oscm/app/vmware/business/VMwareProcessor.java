f0593352369064dc19e6cb64bfc058e2464d9699
worf <worf@estworf.intern.est.fujitsu.com>
2019-02-22 11:31:22 +0100

    Refactor and change the order of showed data

--- oscm-app-vmware/javasrc/org/oscm/app/vmware/business/VMwareProcessor.java
+++ oscm-app-vmware/javasrc/org/oscm/app/vmware/business/VMwareProcessor.java
@@ -1,10 +1,8 @@
 package org.oscm.app.vmware.business;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.oscm.app.vmware.i18n.Messages;
 import org.oscm.app.vmware.remote.vmware.VMClientPool;
 import org.oscm.app.vmware.remote.vmware.VMwareClient;
 import org.slf4j.Logger;
@@ -19,7 +17,6 @@ public class VMwareProcessor {
         String vcenter = ph
                 .getServiceSetting(VMPropertyHandler.TS_TARGET_VCENTER_SERVER);
         VMwareClient vmClient = null;
-        ArrayList<String> publicIp= new ArrayList<String>();
         List<Server> servers = new ArrayList<>();
         ArrayList<String> VMDetails = new ArrayList<String>();
         ArrayList<String> serverInformation = new ArrayList<String>();
@@ -33,35 +30,28 @@ public class VMwareProcessor {
             serverInformation.add("Cluster: " + ph.getTargetCluster());
             serverInformation.add("Datacenter: " + ph.getTargetDatacenter());
             serverInformation.add("VCenter: " + ph.getTargetVCenterServer());
-            
             VM vm = new VM(vmClient, instanceName);
             Server server = new Server();
             server.setStatus(vm.getState(ph).name());
             server.setId(instanceName);
             server.setName(vm.getGuestFullName());
-            publicIp.add(ph.getAccessInfo().toString().replace(instanceName + ", ", ""));
             server.setPublicIP(serverInformation);
             server.setPrivateIP(VMDetails);
-            server.setType(getStringFromList(publicIp));
+            server.setType(ph.getAccessInfo().toString().replace(instanceName + ", ", ""));
             servers.add(server);
-            
         } catch (Exception e) {
             logger.error("Failed to create serverlist");
         }
-        return servers;
-    }
-    
-    public String getStringFromList(List list) {
-        StringBuilder sb = new StringBuilder();
-        for (Iterator<String> iterator = list.iterator(); iterator
-                .hasNext();) {
-            String ip = iterator.next();
-            sb.append(ip);
-            if (iterator.hasNext()) {
-                sb.append("\n");
+        finally {
+            if (vmClient != null) {
+                try {
+                    VMClientPool.getInstance().getPool().returnObject(vcenter,
+                            vmClient);
+                } catch (Exception e) {
+                    logger.error("Failed to return VMware client into pool", e);
+                }
             }
         }
-        return sb.toString();
+        return servers;
     }
-
 }
