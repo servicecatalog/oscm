2fa228397d5e2bb99beb21b99583d8a8d6a8d9ce
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-03-29 14:15:43 +0100

    WIP: Fixing codecov

--- scripts/repush_formatted_code.sh
+++ scripts/repush_formatted_code.sh
@@ -7,7 +7,9 @@ git config --global user.name "Travis CI"
 git config --global push.default simple
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/servicecatalog/oscm.git
 
-if [[ git diff --stat --name-only $(echo $TRAVIS_COMMIT_RANGE) | grep "\.java$" ]] ; then
+GIT_DIFF_OUTPUT=$(git diff --stat --name-only $(echo $TRAVIS_COMMIT_RANGE) | grep "\.java$")
+
+if [ "$GIT_DIFF_OUTPUT" -ne "" ] ; then
     git add *.java
     git commit -m "Applied code formatting [ci skip]"
     git push origin HEAD:$TRAVIS_BRANCH
