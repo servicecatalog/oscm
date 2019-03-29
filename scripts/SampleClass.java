f82689d2437056d3f2f623eb86527ea778962040
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-03-29 14:57:41 +0100

    WIP: Fixing codecov

---
+++ scripts/SampleClass.java
@@ -0,0 +1,27 @@
+
+
+  public class SampleClass {
+     
+    public void testmethod()        {
+    if(true  ) {
+      return;
+    }
+       }
+    
+       
+    
+    
+    
+    
+    
+    
+                public void test() {       
+                          return;
+                     
+                      }
+    
+        
+        
+    
+    
+    }
