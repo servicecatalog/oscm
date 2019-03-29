dfabde92516d023c10071cd4010d6cf81e1bb5dd
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-03-29 14:55:11 +0100

    WIP: Fixing codecov

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -2,14 +2,20 @@
 
 echo "PROCEEDING WITH GOOGLE JAVA FORMAT STEP..."
 
-if [ ! -f libraries/google-java-format.jar ] ; then 
-    curl -o libraries/google-java-format.jar http://central.maven.org/maven2/com/google/googlejavaformat/google-java-format/1.7/google-java-format-1.7-all-deps.jar
-    chmod 755 libraries/google-java-format.jar
-fi
+GIT_DIFF_OUTPUT=$(git diff --stat --name-only $(echo $TRAVIS_COMMIT_RANGE) | grep "\.java$")
 
-git diff --stat --name-only $(echo $TRAVIS_COMMIT_RANGE) | grep "\.java$" | while read -r line ; do
-    echo "FORMATING FILE $line..."
-    java -jar libraries/google-java-format.jar -r $line
-done
+if [[ ! -z "$GIT_DIFF_OUTPUT" ]] ; then
+    if [ ! -f libraries/google-java-format.jar ] ; then 
+        curl -o libraries/google-java-format.jar http://central.maven.org/maven2/com/google/googlejavaformat/google-java-format/1.7/google-java-format-1.7-all-deps.jar
+        chmod 755 libraries/google-java-format.jar
+    fi
+
+    echo $GIT_DIFF_OUTPUT | while read -r line ; do
+        echo "FORMATING FILE $line..."
+        java -jar libraries/google-java-format.jar -r $line
+    done
+else 
+    echo "NOTHING TO FORMAT. ABORTING..."
+fi
 
 echo "DONE!"
