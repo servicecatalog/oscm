05cadeffe2587f95b40c1848c8b011f1ec7e2ff3
Travis CI <travis@travis-ci.org>
2019-04-02 10:03:38 +0000

    Applied code formatting [ci skip]

--- oscm-app/javasrc/org/oscm/app/remote/APPCredentialsUpdateService.java
+++ oscm-app/javasrc/org/oscm/app/remote/APPCredentialsUpdateService.java
@@ -1,4 +1,12 @@
-package org.oscm.app.remote;
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2019
+ *                                                                              
+ *  Creation Date: 02-04-2019                                                      
+ *                                                                              
+ *******************************************************************************/
+
+ package org.oscm.app.remote;
 
 import javax.ejb.Remote;
 
