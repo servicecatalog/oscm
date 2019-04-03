25289968be3c3f813ad36d57162f9096834d4aa0
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-04-03 14:34:41 +0200

    Updated method for storing credentials

--- oscm-app/javasrc/org/oscm/app/v2_0/service/APPCredentialsUpdateServiceBean.java
+++ oscm-app/javasrc/org/oscm/app/v2_0/service/APPCredentialsUpdateServiceBean.java
@@ -1,12 +1,18 @@
 package org.oscm.app.v2_0.service;
 
+import java.security.GeneralSecurityException;
 import java.util.HashMap;
 import java.util.List;
+<<<<<<< bb1f6e20d4e050cde439cde44288fc37bd37152b
 import java.util.Map;
 import java.util.stream.Collectors;
+=======
+
+>>>>>>> Updated method for storing credentials
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+
 import org.oscm.app.remote.APPCredentialsUpdateService;
 import org.oscm.app.v2_0.data.Setting;
 import org.oscm.app.v2_0.exceptions.ConfigurationException;
@@ -20,15 +26,16 @@ public class APPCredentialsUpdateServiceBean implements APPCredentialsUpdateServ
   private static final Logger LOGGER =
       LoggerFactory.getLogger(APPCredentialsUpdateServiceBean.class);
 
+  private static final String PROXY_CONTROLLER_ID = "PROXY";
+
   @EJB protected APPConfigurationServiceBean configService;
 
   @Override
   public String updateUserCredentials(long userId, String username, String password) {
 
-    LOGGER.info(
-        "From logger: Update user credentials: " + userId + ", " + username + ", " + password);
     List<String> controllers = configService.getUserConfiguredControllers(username);
 
+<<<<<<< bb1f6e20d4e050cde439cde44288fc37bd37152b
     Map<String, Setting> settings =
         controllers.stream()
             .filter(controllerId -> controllerId.equals("ess.aws"))
@@ -41,7 +48,39 @@ public class APPCredentialsUpdateServiceBean implements APPCredentialsUpdateServ
           "ess.aws", (HashMap<String, Setting>) settings);
     } catch (ConfigurationException e) { // TODO Auto-generated catch block
       LOGGER.error(e.getMessage(), e);
+=======
+    if (controllers.contains(PROXY_CONTROLLER_ID)) {
+      HashMap<String, String> settings = new HashMap<>();
+      settings.put("BSS_USER_PWD", password);
+      try {
+        configService.storeAppConfigurationSettings(settings);
+        logSuccessInfo(username);
+      } catch (ConfigurationException | GeneralSecurityException e) {
+        LOGGER.error("Unable to store proxy settings " + settings, e.getMessage(), e);
+      }
+>>>>>>> Updated method for storing credentials
     }
+
+    controllers
+        .stream()
+        .filter(id -> !PROXY_CONTROLLER_ID.equals(id))
+        .forEach(
+            id -> {
+              HashMap<String, Setting> settings = new HashMap<>();
+              settings.put("BSS_USER_PWD", new Setting("BSS_USER_PWD", password));
+              try {
+                configService.storeControllerConfigurationSettings(id, settings);
+                logSuccessInfo(username);
+              } catch (ConfigurationException e) {
+                LOGGER.error("Unable to store controller settings " + settings, e.getMessage(), e);
+              }
+            });
+
     return userId + ", " + username + ", " + password;
   }
+  
+  private void logSuccessInfo(String username){
+	  LOGGER.info("User [" + username + "] password updated");
+  }
+ 
 }
