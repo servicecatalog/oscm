c2d265744dd8cbf2d726792d93ce1484bac57f66
worf <worf@estworf.intern.est.fujitsu.com>
2019-04-04 11:49:10 +0200

    Import file via classLoader

--- oscm-app-vmware-unittests/javasrc/org/oscm/app/vmware/importer/ClusterImporterTest.java
+++ oscm-app-vmware-unittests/javasrc/org/oscm/app/vmware/importer/ClusterImporterTest.java
@@ -13,19 +13,27 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
+import java.net.URL;
 
 import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.oscm.app.vmware.business.statemachine.States;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -57,8 +65,7 @@ public class ClusterImporterTest {
             + "<balancer class=\"org.oscm.app.vmware.business.balancer.EquipartitionStorageBalancer\" storage=\"VMdev0,VMdev1\"/>"
             + "</host>"
             + "<storage enabled=\"true\" limit=\"85%\" name=\"VMdev0\"/>"
-            + "<storage enabled=\"true\" limit=\"85%\"/>" 
-            + "</ess:essvcenter>";
+            + "<storage enabled=\"true\" limit=\"85%\"/>" + "</ess:essvcenter>";
 
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
     private final PrintStream originalErr = System.err;
@@ -73,13 +80,18 @@ public class ClusterImporterTest {
         System.setErr(originalErr);
     }
 
+    private File getSource(String filename) throws Exception {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL url = loader.getResource("META-INF/" + filename);
+        File file = new File(url.toURI());
+        return file;
+    }
+
     @Test
-    public void testValidSchema()
-            throws SAXException, IOException, ParserConfigurationException {
+    public void testValidSchema() throws Exception {
 
         // given
-        File file = new File(System.getProperty("user.dir")
-                + "/../../oscm/oscm-app-vmware/javares/META-INF/Loadbalancer_schema.xsd");
+        File file = getSource("Loadbalancer_schema.xsd");
         String constant = XMLConstants.W3C_XML_SCHEMA_NS_URI;
         SchemaFactory xsdFactory = SchemaFactory.newInstance(constant);
         Schema schema = xsdFactory.newSchema(file);
@@ -99,18 +111,17 @@ public class ClusterImporterTest {
 
     @Test
     public void testInvalidSchema()
-            throws SAXException, IOException, ParserConfigurationException {
+            throws Exception {
 
         // given
-        File file = new File(System.getProperty("user.dir")
-                + "/../../oscm/oscm-app-vmware/javares/META-INF/Loadbalancer_schema.xsd");
+        File file = getSource("Loadbalancer_schema.xsd");
         String constant = XMLConstants.W3C_XML_SCHEMA_NS_URI;
         SchemaFactory xsdFactory = SchemaFactory.newInstance(constant);
         Schema schema = xsdFactory.newSchema(file);
         DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
         dfactory.setNamespaceAware(true);
 
-        // when 
+        // when
         dfactory.setValidating(false);
         dfactory.setIgnoringElementContentWhitespace(true);
         dfactory.setSchema(schema);
