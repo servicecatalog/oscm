782d9e616136ccf95200c59caa06e9ffccd2e6f8
Travis CI <travis@travis-ci.org>
2019-04-24 07:28:25 +0000

    Applied code formatting [ci skip]

--- oscm-app-vmware/javasrc/org/oscm/app/vmware/business/PortgroupIpSettings.java
+++ oscm-app-vmware/javasrc/org/oscm/app/vmware/business/PortgroupIpSettings.java
@@ -1,80 +1,77 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  *
- *  Copyright FUJITSU LIMITED 2018
+ * <p>Copyright FUJITSU LIMITED 2018
  *
- *  Creation Date: 2019-04-16
+ * <p>Creation Date: 2019-04-16
  *
- *******************************************************************************/
-
+ * <p>*****************************************************************************
+ */
 package org.oscm.app.vmware.business;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.oscm.app.v2_0.exceptions.APPlatformException;
 
 public class PortgroupIpSettings {
 
-    private VMPropertyHandler ph;
-    private List<String> ips;
-    private static Map<String, Boolean> inUse;
+  private VMPropertyHandler ph;
+  private List<String> ips;
+  private static Map<String, Boolean> inUse;
 
-    PortgroupIpSettings(VMPropertyHandler ph) {
-        this.ph = ph;
-        this.ips = getIpList();
-        if(inUse == null) {
-            inUse = new HashMap<String, Boolean>();
-        }
+  PortgroupIpSettings(VMPropertyHandler ph) {
+    this.ph = ph;
+    this.ips = getIpList();
+    if (inUse == null) {
+      inUse = new HashMap<String, Boolean>();
     }
+  }
 
-    private List<String> getIpList() {
-        String ippools = ph.getIpPoolForPortgroup();
-        String[] ips = ippools.split(",");
-        return Arrays.asList(ips);
-    }
+  private List<String> getIpList() {
+    String ippools = ph.getIpPoolForPortgroup();
+    String[] ips = ippools.split(",");
+    return Arrays.asList(ips);
+  }
 
-    public String getIpAdressFromIpPool() throws APPlatformException {
-        for (int i = 0; ips.size() > i; i++) {
-            if (!ipIsInUse(ips.get(i))) {
-                useIpAdress(ips.get(i));
-                return ips.get(i);
-            }
-        }
-        throw new APPlatformException(
-                "No IP address found Check whether you have specified an IP pool for the port group in the Technical Service. If so, there may not be IPs left in this pool");
+  public String getIpAdressFromIpPool() throws APPlatformException {
+    for (int i = 0; ips.size() > i; i++) {
+      if (!ipIsInUse(ips.get(i))) {
+        useIpAdress(ips.get(i));
+        return ips.get(i);
+      }
     }
-    
-    private boolean ipIsInUse(String ip) {
-        boolean ipExists = inUse.containsKey(ip);
-        if (ipExists) {
-            return inUse.get(ip);
-        } else {
-            inUse.put(ip, false);
-            return false;
-        }
-    }
+    throw new APPlatformException(
+        "No IP address found Check whether you have specified an IP pool for the port group in the Technical Service. If so, there may not be IPs left in this pool");
+  }
 
-    private void useIpAdress(String ip) throws IllegalArgumentException {
-        boolean ipExists = inUse.containsKey(ip);
-        if (!ipExists) {
-            throw new IllegalArgumentException(
-                    "The ip adress " + ip + " is already used");
-        } else {
-            inUse.put(ip, true);
-        }
+  private boolean ipIsInUse(String ip) {
+    boolean ipExists = inUse.containsKey(ip);
+    if (ipExists) {
+      return inUse.get(ip);
+    } else {
+      inUse.put(ip, false);
+      return false;
     }
+  }
 
-    
-    public void returnIpAdressToIpPool(String ip) throws IllegalArgumentException {
-        boolean ipExists = inUse.containsKey(ip);
-        if (ipExists) {
-            inUse.put(ip, false);
-        } else {
-            throw new IllegalArgumentException(
-                    "The ip adress " + ip + " can not be returned. The Ip does not exist");
-        }
+  private void useIpAdress(String ip) throws IllegalArgumentException {
+    boolean ipExists = inUse.containsKey(ip);
+    if (!ipExists) {
+      throw new IllegalArgumentException("The ip adress " + ip + " is already used");
+    } else {
+      inUse.put(ip, true);
     }
-    
+  }
+
+  public void returnIpAdressToIpPool(String ip) throws IllegalArgumentException {
+    boolean ipExists = inUse.containsKey(ip);
+    if (ipExists) {
+      inUse.put(ip, false);
+    } else {
+      throw new IllegalArgumentException(
+          "The ip adress " + ip + " can not be returned. The Ip does not exist");
+    }
+  }
 }
