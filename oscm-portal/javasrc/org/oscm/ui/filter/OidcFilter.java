a950bb3fba95b25c514b8b90191b0217a1d9b2cd
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-07-23 16:12:16 +0200

    Implemented token validation

--- oscm-portal/javasrc/org/oscm/ui/filter/OidcFilter.java
+++ oscm-portal/javasrc/org/oscm/ui/filter/OidcFilter.java
@@ -8,21 +8,16 @@
 
 package org.oscm.ui.filter;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Optional;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.oscm.internal.types.exception.ValidationException;
 import org.oscm.logging.Log4jLogger;
 import org.oscm.logging.LoggerFactory;
 import org.oscm.types.constants.marketplace.Marketplace;
@@ -30,6 +25,18 @@ import org.oscm.types.enumtypes.LogMessageIdentifier;
 import org.oscm.ui.beans.BaseBean;
 import org.oscm.ui.common.Constants;
 import org.oscm.ui.common.JSFUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class OidcFilter implements Filter {
 
@@ -53,10 +60,12 @@ public class OidcFilter implements Filter {
     
     // if request contains id_token, validate it and refresh session with it
     Optional<String> idTokenParam = Optional.ofNullable(httpRequest.getParameter("id_token"));
+    //FIXME: Determine from where should the tenant be retrieved
+    Optional<String> tenantIdParam = Optional.ofNullable(httpRequest.getParameter("tenantId"));
 
     idTokenParam.ifPresent(
         idToken -> {
-          // TODO: validate id token
+          makeTokenValidationRequest(httpRequest, idToken, tenantIdParam);
           httpRequest.getSession().setAttribute(Constants.SESS_ATTR_ID_TOKEN, idToken);
         });
     
@@ -93,20 +102,46 @@ public class OidcFilter implements Filter {
         return;
 
       } else {
-        // TODO: validate id token
+        makeTokenValidationRequest(httpRequest, existingIdToken, tenantIdParam);
       }
     }
 
     chain.doFilter(request, response);
   }
 
-  private boolean isLoginOnMarketplaceRequested(HttpServletRequest request) {
+  private void makeTokenValidationRequest(HttpServletRequest httpRequest, String idToken, Optional<String> tenantIdParam) {
+    String requestedUrl = httpRequest.getRequestURL().toString();
+
+    try {
+      String hostname = new URI(requestedUrl).getHost();
+      String resourceUrl = "http://" + hostname + ":9090/oscm-identity/verify_token";
+
+      CloseableHttpClient client = HttpClients.createDefault();
+      HttpPost post = new HttpPost(resourceUrl);
+
+      List<NameValuePair> params = new ArrayList<NameValuePair>(3);
+      params.add(new BasicNameValuePair("token", idToken));
+      post.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+      HttpResponse validationResponse = client.execute(post);
+      if (validationResponse.getStatusLine().getStatusCode() != Response.Status.OK.getStatusCode()) {
+        throw new ValidationException("Token validation failed!");
+      }
+
+    } catch (URISyntaxException | IOException | ValidationException e) {
+        LOGGER.logError(
+          Log4jLogger.SYSTEM_LOG,
+          e,
+          LogMessageIdentifier.ERROR_AUTH_REQUEST_GENERATION_FAILED);
+    }
+
+    private boolean isLoginOnMarketplaceRequested(HttpServletRequest request) {
 
     boolean isMarketplacePage = request.getServletPath().contains(Marketplace.MARKETPLACE_ROOT);
     Optional<String> loginRequested = Optional.ofNullable(request.getParameter("login"));
 
     return isMarketplacePage && loginRequested.isPresent();
-  }
+    }
 
   @Override
   public void destroy() {}
