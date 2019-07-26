0c020f85b0e848644e368f753a9ffc037badce13
worf <worf@estworf.intern.est.fujitsu.com>
2019-07-26 08:38:46 +0200

    Implement the retriving of userdata for an existing user

---
+++ oscm-identitymgmt/javasrc/org/oscm/identityservice/model/UserinfoModel.java
@@ -0,0 +1,82 @@
+package org.oscm.identityservice.model;
+
+
+public class UserinfoModel {
+    
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String city;
+    private String address;
+    private String phone;
+    private String locale;
+    private String gender;
+    private String postalCode;
+    
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getLocale() {
+        return locale;
+    }
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
+
+
+
+
