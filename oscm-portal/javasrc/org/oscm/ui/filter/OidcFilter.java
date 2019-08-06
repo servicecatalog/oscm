4408d44d96796c174d4e7ee872bc93b1f0a8ca37
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-08-06 15:08:19 +0200

    Changed protocol for internal token validation back to HTTP

--- oscm-portal/javasrc/org/oscm/ui/filter/OidcFilter.java
+++ oscm-portal/javasrc/org/oscm/ui/filter/OidcFilter.java
@@ -134,8 +134,7 @@ public class OidcFilter extends BaseBesFilter implements Filter {
 
     HttpResponse validationResponse = null;
     String hostname = new URI(requestedUrl).getHost();
-    // TODO adapt for HTTPS protocol and port
-    String resourceUrl = "https://" + hostname + ":9090/oscm-identity/verify_token";
+    String resourceUrl = "http://" + hostname + ":9090/oscm-identity/verify_token";
 
     CloseableHttpClient client = HttpClients.createDefault();
     HttpPost post = new HttpPost(resourceUrl);
@@ -176,7 +175,7 @@ public class OidcFilter extends BaseBesFilter implements Filter {
       StringBuffer bf = new StringBuffer();
 
       // TODO adapt for HTTPS protocol and port
-      bf.append(String.format("https://%s:9090/oscm-identity/login?", hostname));
+      bf.append(String.format("https://%s:9091/oscm-identity/login?", hostname));
       bf.append("state=");
       bf.append(getRequestedURL());
 
