dba671b27d50d205d64b2dab46ce8ee8667648c2
goebel <lorenz.goebel@est.fujitsu.com>
2019-11-26 12:42:44 +0100

    Cleaned up.

--- oscm-devruntime/javasrc/org/oscm/dbtask/MigrateOrganizationNames.java
+++ oscm-devruntime/javasrc/org/oscm/dbtask/MigrateOrganizationNames.java
@@ -11,38 +11,37 @@ package org.oscm.dbtask;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
+ * Migrate organizations without name.
+ * <p>
+ * 
  * @author goebel
- *
  */
 public class MigrateOrganizationNames extends DatabaseUpgradeTask {
-
-    private Connection conn;
     private static final String QUERY_ALL_ANONYMOUS_ORGS = "SELECT * FROM organization WHERE name = NULL or name = '';";
     private static final String QUERY_SET_NAME = "UPDATE organization SET name='%s' WHERE tkey=%s;";
     
     @Override
     public void execute() throws Exception {
-        conn = getConnection();
+        
+        final Connection conn = getConnection();
         PreparedStatement pstmt = conn.prepareStatement(QUERY_ALL_ANONYMOUS_ORGS);
         
-        Map<String, String> toUpdate = new HashMap<String, String>();
         ResultSet rs = pstmt.executeQuery();
         while (rs.next()) {
-
             String name = rs.getString("name");
             String tkey = String.valueOf(rs.getLong("tkey"));
             String orgid = rs.getString("organizationid");
-            if (name == null || name.trim().length() == 0) {
+            if (isBlank(name)) {
                 String query = String.format(QUERY_SET_NAME, orgid, tkey);
-                PreparedStatement pstmtUpdate = conn.prepareStatement(query);
-                pstmtUpdate.executeUpdate();
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.executeUpdate();
             }
         }
-
     }
-
+    
+    private boolean isBlank(String name) {
+        return (name == null || name.trim().length() == 0);
+    }
 }
