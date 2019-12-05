c08e9300d37cbcd91cca7e438ab2bbdc90f7820f
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-12-05 07:33:03 +0100

    fixup! Added some dummy classes to test coverage delta

--- oscm-converter-unittests/javasrc/org/oscm/converter/SomeDummyClassToDeleteTest.java
+++ oscm-converter-unittests/javasrc/org/oscm/converter/SomeDummyClassToDeleteTest.java
@@ -8,4 +8,9 @@ public class SomeDummyClassToDeleteTest {
         public void method1Test() {
                 new SomeDummyClassToDelete().method1();
         }
+
+        @Test
+        public void method2Test() {
+                new SomeDummyClassToDelete().method2();
+        }
 }
