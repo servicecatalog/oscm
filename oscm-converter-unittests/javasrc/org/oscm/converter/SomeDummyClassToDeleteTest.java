be7e7a8296b4eda8561955df327b56b33f2ed982
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-12-05 13:18:16 +0100

    TestCommit

--- oscm-converter-unittests/javasrc/org/oscm/converter/SomeDummyClassToDeleteTest.java
+++ oscm-converter-unittests/javasrc/org/oscm/converter/SomeDummyClassToDeleteTest.java
@@ -4,13 +4,5 @@ import org.junit.Test;
 
 public class SomeDummyClassToDeleteTest {
 
-        @Test
-        public void method1Test() {
-                new SomeDummyClassToDelete().method1();
-        }
-
-        @Test
-        public void method2Test() {
-                new SomeDummyClassToDelete().method2();
-        }
+        
 }
