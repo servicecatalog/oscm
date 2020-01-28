45617abf9b0cce98278200bc3b2e177e29833af4
KabacinskiPiotr <piotr.kabacinski@ts.fujitsu.com>
2020-01-28 12:50:20 +0100

    Create one common module UI tests

---
+++ oscm-ui-tests/src/test/java/portal/PortalMarketplaceWT.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2018                                           
+ *                                                                                                                                 
+ *  Creation Date: Feb 8, 2017                                                      
+ *                                                                              
+ *******************************************************************************/
+
+package portal;
+
+import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import webtest.PortalHtmlElements;
+import webtest.PortalPathSegments;
+import webtest.PortalTester;
+
+/**
+ * Integration web test to create an new marketplace.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PortalMarketplaceWT {
+
+    @Rule
+    public TestWatcher testWatcher = new JUnitHelper();
+
+    private static final String MARKETPLACE = "mp_"
+            + PlaygroundSuiteTest.currentTimestampe;
+    private String marketplaceId;
+    private static PortalTester tester;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        tester = new PortalTester();
+        String userid = tester.getPropertie(PortalTester.BES_ADMIN_USER_ID);
+        String userpassword = tester
+                .getPropertie(PortalTester.BES_ADMIN_USER_PWD);
+        tester.loginPortal(userid, userpassword);
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        tester.logoutPortal();
+        tester.close();
+    }
+
+    @Test
+    public void test01create() throws Exception {
+        marketplaceId = "";
+
+        tester.visitPortal(PortalPathSegments.CREATE_MARKETPLACE);
+
+        tester.writeValue(PortalHtmlElements.CREATE_MARKETPLACE_INPUT_NAME,
+                MARKETPLACE);
+        tester.writeValue(PortalHtmlElements.CREATE_MARKETPLACE_INPUT_ORG_ID,
+                PlaygroundSuiteTest.supplierOrgId);
+
+        tester.clickElement(PortalHtmlElements.CREATE_MARKETPLACE__BUTTON_SAVE);
+
+        Assert.assertTrue(tester.getExecutionResult());
+        PlaygroundSuiteTest.marketPlaceId = MARKETPLACE;
+    }
+
+    // @Test
+    public void remove() throws Exception {
+
+        marketplaceId = tester.getCreatedId(tester.readInfoMessage());
+
+        if (marketplaceId == null || marketplaceId == "")
+            throw new Exception(
+                    "Marketplace " + MARKETPLACE + " doesn't exists!");
+
+        tester.visitPortal(PortalPathSegments.DELETE_MARKETPLACE);
+        tester.selectDropdown(
+                PortalHtmlElements.DELETE_MARKETPLACE_DROPDOWN_IDLIST,
+                marketplaceId);
+        tester.waitForElement(
+                By.id(PortalHtmlElements.DELETE_MARKETPLACE_BUTTON_DELETE), 10);
+        tester.clickElement(
+                PortalHtmlElements.DELETE_MARKETPLACE_BUTTON_DELETE);
+        tester.waitForElement(
+                By.id(PortalHtmlElements.DELETE_MARKETPLACE_BUTTON_CONFIRM),
+                10);
+        tester.clickElement(
+                PortalHtmlElements.DELETE_MARKETPLACE_BUTTON_CONFIRM);
+
+        Assert.assertTrue(tester.getExecutionResult());
+    }
+
+}
---
+++ oscm-ui-tests/src/test/java/portal/PortalTechServiceWT.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2018                                           
+ *                                                                                                                                 
+ *  Creation Date: Feb 8, 2017                                                      
+ *                                                                              
+ *******************************************************************************/
+
+package portal;
+
+import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import webtest.PortalHtmlElements;
+import webtest.PortalPathSegments;
+import webtest.PortalTester;
+
+/**
+ * Integration web test to create a technical service.
+ * 
+ * @author sxu
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PortalTechServiceWT {
+
+    private static final String FILE_PATH_IMPORT_TECHSERVICE = "technicalservice.xml.path";
+    private static final String IMPORT_TECHSERV_NAME = "technicalservice.name";
+
+    private static PortalTester tester;
+
+    @Rule
+    public TestWatcher testWatcher = new JUnitHelper();
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        tester = new PortalTester();
+        String userid = PlaygroundSuiteTest.supplierOrgAdminId;
+        String userpassword = PlaygroundSuiteTest.supplierOrgAdminPwd;
+        tester.loginPortal(userid, userpassword);
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        tester.logoutPortal();
+        tester.close();
+    }
+
+    @Test
+    public void test01importTechService() throws Exception {
+
+        tester.visitPortal(PortalPathSegments.IMPORT_TECHNICALSERVICE);
+
+        String pathFile = System.getProperty(FILE_PATH_IMPORT_TECHSERVICE);
+        if (pathFile == null) {
+            pathFile = tester.getPropertie(FILE_PATH_IMPORT_TECHSERVICE);
+        }
+        tester.log("Import file " + pathFile + " as technical service.");
+        tester.getDriver()
+                .findElement(By
+                        .id(PortalHtmlElements.IMPORT_TECHSERVICE_UPLOAD_INPUT))
+                .sendKeys(pathFile);
+        tester.wait(5);
+        tester.clickElement(
+                PortalHtmlElements.IMPORT_TECHSERVICE_UPLOAD_BUTTON);
+
+        Assert.assertTrue(tester.getExecutionResult());
+
+    }
+
+    @Test
+    public void test02updateTechService() throws Exception {
+
+        String importTechSerName = tester.getPropertie(IMPORT_TECHSERV_NAME);
+        tester.visitPortal(PortalPathSegments.UPDATE_TECHNICALSERVICE);
+        Select dropdownServiceName = new Select(tester.getDriver().findElement(
+                By.id(PortalHtmlElements.UPDATE_TECHSERVICE_DROPDOWN_SERVICENAME)));
+        dropdownServiceName.selectByVisibleText(importTechSerName);
+
+        tester.waitForElementVisible(
+                By.id(PortalHtmlElements.UPDATE_TECHSERVICE_BUTTONLINK_SAVE),
+                10);
+        setParamDescription("APP_CONTROLLER_ID",
+                PlaygroundSuiteTest.controllerId);
+        setParamDescription("PARAM_EMAIL",
+                PortalTester.TECHSERVICE_PARAM_EMAIL);
+        setParamDescription("PARAM_USER", PortalTester.TECHSERVICE_PARAM_USER);
+        setParamDescription("PARAM_PWD", PortalTester.TECHSERVICE_PARAM_PWD);
+        setParamDescription("PARAM_MESSAGETEXT",
+                PortalTester.TECHSERVICE_PARAM_MESSAGETEXT);
+
+        tester.clickElement(
+                PortalHtmlElements.UPDATE_TECHSERVICE_BUTTONLINK_SAVE);
+        tester.waitForElement(
+                By.id(PortalHtmlElements.PORTAL_SPAN_INFOS), 10);
+        Assert.assertTrue(tester.getExecutionResult());
+        PlaygroundSuiteTest.techServiceName = importTechSerName;
+    }
+
+    private void setParamDescription(String paramId, String paramDescription) {
+        String descriptionXpath = "//table[@id='"
+                + PortalHtmlElements.UPDATE_TECHSERVICE_PARAM_TABLE
+                + "']//span[.= '" + paramId + "']/../../td[2]/input";
+        tester.getDriver().findElement(By.xpath(descriptionXpath)).clear();
+        tester.getDriver().findElement(By.xpath(descriptionXpath))
+                .sendKeys(paramDescription);
+    }
+
+}
