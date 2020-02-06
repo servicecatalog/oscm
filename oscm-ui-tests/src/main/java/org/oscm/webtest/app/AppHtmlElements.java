4f33ac9324b61cd8b40c45193205c89218d503dd
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-02-06 13:57:48 +0100

    UI test for /oscm-app

--- oscm-ui-tests/src/main/java/org/oscm/webtest/app/AppHtmlElements.java
+++ oscm-ui-tests/src/main/java/org/oscm/webtest/app/AppHtmlElements.java
@@ -7,18 +7,22 @@
  *
  * <p>*****************************************************************************
  */
-package org.oscm.webtest;
+package org.oscm.webtest.app;
 
 public class AppHtmlElements {
   public static final String APP_CONFIG_DIV_CLASS_STATUS_MSG = "statusPanel";
   public static final String APP_CONFIG_LICLASS_STATUS_MSG_OK = "statusGreen";
   public static final String APP_CONFIG_LICLASS_STATUS_MSG_ERROR = "statusRed";
 
+  public static String TEST_CONTROLLER_ID = "a.ess.sample";
+
   public static final String APP_CONFIG_FORM1 = "configurationSettings";
   public static final String APP_CONFIG_FORM1_INPUT_END_NEWCONTROLLERID = ":newControllerId";
   public static final String APP_CONFIG_FORM1_INPUT_END_NEWORGID = ":newOrgnizationId";
   public static final String APP_CONFIG_FORM1_INPUT_END_EXISTORGID = ":configurationValue";
   public static final String APP_CONFIG_FORM_BUTTON_CLASS = "oscm_app_button";
+  public static final String APP_SAVE_CONFIG_CONTROLLER_BUTTON_CLASS = "configurationSettings:j_idt62";
+  public static final String APP_CONTROLLER_TABLE_FIELD = "configurationSettings:j_idt52:0:j_idt53";
 
   public static final String APP_CONFIG_FORM2 = "appSettings";
 
@@ -26,4 +30,5 @@ public class AppHtmlElements {
   public static final String APP_SERVICEINSTANCE_TABLE_ID = "serviceForm:serviceTable";
 
   public static final String APP_SAMPLECONTROLLER_FORM_ID = "configurationSettings";
+
 }
---
+++ oscm-ui-tests/src/test/java/org/oscm/app/AppConfigurationWT.java
@@ -0,0 +1,90 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2018
+ *
+ * <p>Creation Date: 20 6, 2018
+ *
+ * <p>*****************************************************************************
+ */
+package org.oscm.app;
+
+import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runners.MethodSorters;
+import org.oscm.portal.JUnitHelper;
+import org.oscm.portal.PlaygroundSuiteTest;
+import org.oscm.webtest.app.AppHtmlElements;
+import org.oscm.webtest.app.AppTester;
+
+import static org.junit.Assert.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AppConfigurationWT {
+
+  private static AppTester tester;
+
+  @Rule public TestWatcher testWatcher = new JUnitHelper();
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    tester = new AppTester();
+    String userid = tester.getPropertie(AppTester.APP_ADMIN_USER_ID);
+    String userpassword = tester.getPropertie(AppTester.APP_ADMIN_USER_PWD);
+    tester.loginAppConfig(userid, userpassword);
+  }
+
+  @AfterClass
+  public static void cleanUp() throws Exception {
+    tester.logoutAppConfig();
+    tester.close();
+  }
+
+  @Test
+  public void test01setConfiguration() throws Exception {
+
+    tester.getContentAppConfigTable("formID");
+
+    tester.setAppAdminMailAddress(PlaygroundSuiteTest.supplierOrgAdminMail);
+    tester.setBssUserId(tester.getPropertie(AppTester.APP_ADMIN_USER_ID));
+    tester.setBssUserKey("10");
+    System.out.println(AppTester.APP_ADMIN_USER_ID + ":::::::::" + AppTester.APP_ADMIN_USER_PWD);
+//    tester.setBssUserPwd(tester.getPropertie(AppTester.APP_ADMIN_USER_PWD));
+
+    assertEquals(PlaygroundSuiteTest.supplierOrgAdminMail, tester.returnValueFromAppSettings(1));
+    assertEquals(AppTester.APP_ADMIN_USER_ID, tester.returnValueFromAppSettings(3));
+    assertEquals("10", tester.returnValueFromAppSettings(4));
+//    assertEquals(AppTester.APP_ADMIN_USER_PWD, tester.returnValueFromAppSettings(5));
+  }
+
+  @Test
+  public void test02createNewControllerId() throws Exception {
+
+    try {
+      tester.registerController(AppHtmlElements.TEST_CONTROLLER_ID, PlaygroundSuiteTest.supplierOrgId);
+    } catch (Exception e) {
+      if (e.getMessage() != null
+          && e.getMessage().contentEquals(AppTester.ERROR_MSG_CONTROLLER_EXISTS)) {
+        tester.changeOrgIdOnController(AppHtmlElements.TEST_CONTROLLER_ID, PlaygroundSuiteTest.supplierOrgId);
+      }
+    }
+
+    assertTrue(tester.readInfoMessage().contains("saved successfully"));
+  }
+
+  @Test
+  public void test03removeControllerId() {
+    tester.removeCreatedController();
+
+    assertNotEquals(
+        AppHtmlElements.TEST_CONTROLLER_ID, tester.readValue(AppHtmlElements.APP_CONTROLLER_TABLE_FIELD));
+  }
+
+  @Test
+  public void test04checkConnection() {
+    tester.removeCreatedController();
+
+    assertNotEquals(
+        AppHtmlElements.TEST_CONTROLLER_ID, tester.readValue("configurationSettings:j_idt52:0:j_idt53"));
+  }
+}
