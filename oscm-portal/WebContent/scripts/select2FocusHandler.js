b02f177309fa36b3b8a2264e6787cec6ef3a38bf
Georgia-Anna Farmaki <georgia-anna.farmaki@est.fujitsu.com>
2020-02-18 15:42:34 +0100

    Fixes #631 - Split Dirty/Label highlight behavior.

---
+++ oscm-portal/WebContent/scripts/select2FocusHandler.js
@@ -0,0 +1,19 @@
+/* 
+ *  Copyright FUJITSU LIMITED 2020
+ */ 
+
+  $(document).ready(function () {
+	     
+      $(document).on('select2:open', '.select2style', function (e) {
+		  AdmUtils.setFocus(e.target,true);
+		});
+      
+      $(document).on('select2:select', '.select2style', function (e) {
+		  AdmUtils.setFocus(e.target,true);
+		});
+      
+      $(document).on('blur', '.select2-selection--single', function (e) {
+		  AdmUtils.setFocus(e.target,false);
+		});
+
+  });
