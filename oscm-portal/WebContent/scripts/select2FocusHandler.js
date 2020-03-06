3ed81d4438768a6f419057b99bb721b25fe0ed07
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-06 11:03:49 +0100

    Test 04

--- oscm-portal/WebContent/scripts/select2FocusHandler.js
+++ oscm-portal/WebContent/scripts/select2FocusHandler.js
@@ -3,11 +3,12 @@
  */
   $(document).ready(function () {
 
-      var element = $(document).getElementById('#{id}');
-      try {element.focus();
-           $(document).getElementById('contentPanelDiv').scrollIntoView(true);
-          } catch (e) {/* if the element cannot get the focus, ignore it */}
-	     
+      var element = document.getElementById('id');
+      try {
+            element.focus();
+            document.getElementById('contentPanelDiv').scrollIntoView(true);
+      } catch (e) {/* if the element cannot get the focus, ignore it */}
+
       $(document).on('select2:open', '.select2style', function (e) {
 		  AdmUtils.setFocus(e.target,true);
 		});
