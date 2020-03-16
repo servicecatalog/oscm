c29a81256f4a412b6688c8bd5e39871b9cac999d
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-16 07:36:34 +0100

    Test 04

--- oscm-portal/WebContent/scripts/bootsFacesUtils.js
+++ oscm-portal/WebContent/scripts/bootsFacesUtils.js
@@ -5,23 +5,29 @@ var BootsFacesUtils = {
 };
 
 BootsFacesUtils.preventChangeSelectionForDropDown = function() {
+alert("Wszedem do funckji prevent");
     $(document).on('select2:selecting',function (e) {
        if (AdmUtils.isNotDirtyOrConfirmed()) {
+       alert("Wszedem do ifa");
            AdmUtils.storeValue(this);
            $(document).off('select2:selecting');
        } else {
+       alert("Wszedem do else");
            e.preventDefault();
        }
     });
 }
 
 BootsFacesUtils.changeSelectionIndexAtDropDown = function(dropDownID) {
+alert("Wszedem do funckji prevent");
     $(document).on('select2:selecting', function (e) {
     if(AdmUtils.isNotDirtyOrConfirmed())
-        {#{rich:element('selectedKey')}.value = dropDownID.options[this.selectedIndex].value;
+           alert("Wszedem do ifa");
+        {#{rich:element('selectedKey')}.value = dropDownID.options[dropDownID.selectedIndex].value;
          #{rich:element('selectForm')}.submit();}
          $(document).off('select2:selecting');
     else {
+           alert("Wszedem do else");
         e.preventDefault();
     }
     });
