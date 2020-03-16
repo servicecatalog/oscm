3fd009c3e107dd97509d36daa60496df7c2d96f7
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-16 11:25:51 +0100

    Test 25

--- oscm-portal/WebContent/scripts/bootsFacesUtils.js
+++ oscm-portal/WebContent/scripts/bootsFacesUtils.js
@@ -15,12 +15,12 @@ BootsFacesUtils.preventChangeSelectionForDropDown = function() {
 }
 
 BootsFacesUtils.changeSelectionIndexAtDropDown = function(dropDownID) {
-    $(document).on('select2:selecting', function (e) {
-        if(AdmUtils.isNotDirtyOrConfirmed()) {
-        var selValue = $("#techServiceId").val();
+var selValue = $("#techServiceId").val();
         alert ("Value to: " + selValue + " a dropDOwn to: " + dropDownID)
             document.getElementById("selectedKey").value=selValue.options[selValue.selectedIndex].value;
            document.forms["selectForm"].submit();
+    $(document).on('select2:selecting', function (e) {
+        if(AdmUtils.isNotDirtyOrConfirmed()) {
            $(document).off('select2:selecting');
         } else {
            AdmUtils.restoreValue(dropDownID);
