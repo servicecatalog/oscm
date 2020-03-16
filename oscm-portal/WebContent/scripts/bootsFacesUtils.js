a822dab0f1f8bf6eec303bee572e0161a4ef31cf
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-16 08:16:32 +0100

    TEts 07

--- oscm-portal/WebContent/scripts/bootsFacesUtils.js
+++ oscm-portal/WebContent/scripts/bootsFacesUtils.js
@@ -23,10 +23,10 @@ alert("Wszedem do funckji prevent");
     $(document).on('select2:selecting', function (e) {
     if(AdmUtils.isNotDirtyOrConfirmed()) {
            alert("Wszedem do ifa");
-//           var conceptName = $(dropDownID).find(":selected").text();
-       #{rich:element('selectedKey')}.value = dropDownID.options[dropDownID.selectedIndex].value;
-//       document.forms[myFormName].submit();
-         #{rich:element('selectForm')}.submit();
+           document.element['selectedKey'].value = $(dropDownID).find(":selected").text();
+//         document.element('selectedKey')}.value = dropDownID.options[dropDownID.selectedIndex].value;
+       document.forms['selectForm'].submit();
+//         #{rich:element('selectForm')}.submit();
          $(document).off('select2:selecting');}
     else {
            alert("Wszedem do else");
