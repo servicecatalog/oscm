8c429002f0b95a89c15b4956154973d2a6291d61
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-16 14:11:32 +0100

    Test 44

--- oscm-portal/WebContent/scripts/bootsFacesUtils.js
+++ oscm-portal/WebContent/scripts/bootsFacesUtils.js
@@ -14,7 +14,7 @@ BootsFacesUtils.preventChangeSelectionForDropDown = function() {
     });
 }
 
-BootsFacesUtils.changeSelectionIndexAtDropDown = functione, element) {
+BootsFacesUtils.changeSelectionIndexAtDropDown = function(e, element) {
     var i = element.selectedIndex;
 	var o = element.options[i];
 	var input = document.getElementById("selectForm:selectedKey");
