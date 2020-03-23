3391298f94affeb8b5d0695e7f0d22f07c34b735
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-23 09:26:31 +0100

    Test 24

--- oscm-portal/WebContent/scripts/focusBootsFaces.js
+++ oscm-portal/WebContent/scripts/focusBootsFaces.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
             var elements = document.getElementsByClassName("select2-selection__rendered");
             var elementClicked = false;
-            $(document).on('select2-opening', function(e) {
+            $(document).on('select2:opening', function(e) {
                 elementClicked = true;
+                alert("select");
                 for (var i = 0; i < elements.length; i++) {
                     AdmUtils.setFocus(elements[i], false);
                 }
 		        AdmUtils.setFocus(e.target, true);
 		    });
             document.addEventListener('click', function() {
+                            alert("click");
 		         if(!elementClicked) {
                     for (var i = 0; i < elements.length; i++) {
                         AdmUtils.setFocus(elements[i], false);
