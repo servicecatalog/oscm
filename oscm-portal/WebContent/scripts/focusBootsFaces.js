84264401b6e5e1a954f79289483aec7aaa23da78
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-23 10:13:06 +0100

    Test 30

--- oscm-portal/WebContent/scripts/focusBootsFaces.js
+++ oscm-portal/WebContent/scripts/focusBootsFaces.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
   var elements = document.getElementsByClassName("select2-selection__rendered");
+  var elementClicked = false;
   $(document).on('select2:opening', function(e) {
+    for (var i = 0; i < elements.length; i++) {
+      AdmUtils.setFocus(elements[i], false);
+    }
 	AdmUtils.setFocus(e.target, true);
+    elementClicked = true;
   });
     document.addEventListener('click', function() {
+	  if(!elementClicked) {
         for (var i = 0; i < elements.length; i++) {
            AdmUtils.setFocus(elements[i], false);
         }
+      }
+      elementClicked = false;
 	});
 });
