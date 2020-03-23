cc194a946327db0ca629c3be66640c73b4bcc0cd
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-03-23 10:56:05 +0100

    Test 33

--- oscm-portal/WebContent/scripts/bootsFacesUtils.js
+++ oscm-portal/WebContent/scripts/bootsFacesUtils.js
@@ -1,38 +1,38 @@
 //  Copyright FUJITSU LIMITED 2020
 
 var BootsFacesUtils = {
-	storedForm: []
+  storedForm: []
 };
 
 BootsFacesUtils.preventChangeSelectionForDropDown = function() {
-    $(document).on('select2:selecting', function(e) {
-       if (AdmUtils.isNotDirtyOrConfirmed()) {
-           $(document).off('select2:selecting');
-       } else {
-           e.preventDefault();
-       }
-    });
+  $(document).on('select2:selecting', function(e) {
+    if (AdmUtils.isNotDirtyOrConfirmed()) {
+      $(document).off('select2:selecting');
+    } else {
+      e.preventDefault();
+    }
+  });
 }
 
 BootsFacesUtils.preventChangeSelectionForSpecificDropDown = function(item) {
-    var itemID = "[id='" + item + "']";
-      $(itemID).on('select2:selecting', function (e) {
-        if (AdmUtils.isNotDirtyOrConfirmed()) {
-           setDirty(false);
-        } else {
-            e.preventDefault();
-        }
-      });
+  var itemID = "[id='" + item + "']";
+  $(itemID).on('select2:selecting', function (e) {
+    if (AdmUtils.isNotDirtyOrConfirmed()) {
+      setDirty(false);
+    } else {
+      e.preventDefault();
+    }
+  });
 }
 
 BootsFacesUtils.changeSelectionIndexAtDropDown = function(element) {
-       var i = element.selectedIndex;
-       var o = element.options[i];
-       var input = document.getElementById("selectForm:selectedKey");
-       if (input != null) {
-           input.value = o.value;
-       }
-       document.getElementById('selectForm').submit();
+  var i = element.selectedIndex;
+  var o = element.options[i];
+  var input = document.getElementById("selectForm:selectedKey");
+  if (input != null) {
+    input.value = o.value;
+  }
+  document.getElementById('selectForm').submit();
 }
 
 BootsFacesUtils.focusLabelNextToDropDown = function() {
