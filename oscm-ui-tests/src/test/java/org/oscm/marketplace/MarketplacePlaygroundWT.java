300c777d5c7093ec300cbaf9e73b4303908acb40
mchudy-fujitsu <58510012+mchudy-fujitsu@users.noreply.github.com>
2020-03-31 16:14:20 +0200

    fixup! marketplace playground logout

--- oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplacePlaygroundWT.java
+++ oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplacePlaygroundWT.java
@@ -24,8 +24,7 @@ public class MarketplacePlaygroundWT {
     tester = new PortalTester();
     String userid = PlaygroundSuiteTest.supplierOrgAdminId;
     String userpassword = PlaygroundSuiteTest.supplierOrgAdminPwd;
-//    tester.loginMarketplacePlayground(userid, userpassword, PlaygroundSuiteTest.marketPlaceId);
-    tester.loginMarketplacePlayground("administrator", "admin123", "a20d1efe");
+    tester.loginMarketplacePlayground(userid, userpassword, PlaygroundSuiteTest.marketPlaceId);
   }
 
   @AfterClass
