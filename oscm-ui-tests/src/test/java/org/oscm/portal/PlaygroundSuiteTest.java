f19f8543ec127b3d43faee1e066dc62b347b6744
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-04-03 13:02:47 +0200

    Rebase with master

--- oscm-ui-tests/src/test/java/org/oscm/portal/PlaygroundSuiteTest.java
+++ oscm-ui-tests/src/test/java/org/oscm/portal/PlaygroundSuiteTest.java
@@ -13,7 +13,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.oscm.app.*;
+import org.oscm.marketplace.MarketplaceAccountWT;
 import org.oscm.marketplace.MarketplacePlaygroundWT;
+import org.oscm.marketplace.MarketplaceSubscriptionWT;
 import org.oscm.webtest.WebTester;
 
 /**
