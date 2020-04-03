47883d6b2f95c32d3921f02e79ad471a7312249a
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-04-03 16:34:00 +0200

    Implement Lorenz change request

--- oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplaceAccountWT.java
+++ oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplaceAccountWT.java
@@ -41,14 +41,14 @@ public class MarketplaceAccountWT {
     tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Jacob");
     tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
     tester.writeValue(
-        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "jacob.smith@mail.com");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "jacob.smith@email.com");
     tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "en");
     tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_BUTTON);
     tester.writeValue(
         MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_FIELD, "Smiths organization");
     tester.writeValue(
         MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_EMAIL_FIELD,
-        "smiths.organization@mail.com");
+        "smiths.organization@email.com");
     tester.writeValue(
         MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_ADDRESS_FIELD,
         "Australia\nSmall Village 10");
@@ -75,7 +75,7 @@ public class MarketplaceAccountWT {
     tester.writeValue(
         MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_NAME_FIELD, "Smiths organization");
     tester.writeValue(
-        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_EMAIL_FIELD, "smiths.organization@mail.com");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_EMAIL_FIELD, "smiths.organization@email.com");
     tester.writeValue(
         MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_ADDRESS_FIELD,
         "Australia\nSmall Village 10");
@@ -84,7 +84,7 @@ public class MarketplaceAccountWT {
     assertTrue(tester.readInfoMessage().contains("The billing address has been successfully save"));
   }
 
-//  @Test
+  @Test
   public void test03addPaymentType() throws InterruptedException {
     tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
     tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_BUTTON);
@@ -98,7 +98,7 @@ public class MarketplaceAccountWT {
         tester.readInfoMessage().contains("The payment information has been successfully saved"));
   }
 
-//  @Test
+  @Test
   public void test04addUser() throws InterruptedException {
     tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
     tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_SHOW_BUTTON);
@@ -107,7 +107,7 @@ public class MarketplaceAccountWT {
     tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Isabella");
     tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
     tester.writeValue(
-        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "isabella.smith@mail.com");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "isabella.smith@email.com");
     tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_USERID_FIELD, "isabelSmith");
     tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "de");
     tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_CREATE_BUTTON);
@@ -118,7 +118,7 @@ public class MarketplaceAccountWT {
             .contains("The user isabelSmith has been successfully registered."));
   }
 
-//  @Test
+  @Test
   public void test05addUserRoles() throws InterruptedException {
     tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_ADD_ROLE_CHECKBOX);
     tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_ROLE_ASSIGN_BUTTON);
