274ef920018ee266278b78809f6f2f1b7a66539d
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-04-03 13:38:09 +0200

    Clean code

--- oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplaceAccountWT.java
+++ oscm-ui-tests/src/test/java/org/oscm/marketplace/MarketplaceAccountWT.java
@@ -6,7 +6,7 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.oscm.portal.JUnitHelper;
 import org.oscm.portal.PlaygroundSuiteTest;
-import org.oscm.webtest.PortalHtmlElements;
+import org.oscm.webtest.MarketplaceHtmlElements;
 import org.oscm.webtest.PortalTester;
 
 import static org.junit.Assert.assertTrue;
@@ -35,64 +35,64 @@ public class MarketplaceAccountWT {
   @Test
   public void test01fillPersonalData() {
 
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_SHOW_BUTTON);
-    tester.selectDropdown(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_TITLE_DROPDOWN, "MR");
-    tester.writeValue(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Jacob");
-    tester.writeValue(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_SHOW_BUTTON);
+    tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_TITLE_DROPDOWN, "MR");
+    tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Jacob");
+    tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "jacob.smith@mail.com");
-    tester.selectDropdown(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "en");
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_BUTTON);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "jacob.smith@mail.com");
+    tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "en");
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_BUTTON);
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_FIELD, "Smiths organization");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_FIELD, "Smiths organization");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_EMAIL_FIELD,
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_EMAIL_FIELD,
         "smiths.organization@mail.com");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_ADDRESS_FIELD,
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_ORGANIZATION_ADDRESS_FIELD,
         "Australia\nSmall Village 10");
-    tester.selectDropdown(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_COUNTRY_DROPDOWN, "AU");
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_SAVE_BUTTON);
+    tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_COUNTRY_DROPDOWN, "AU");
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_SAVE_BUTTON);
 
     assertTrue(tester.readInfoMessage().contains("Your profile has been successfully saved"));
-    assertTrue(tester.verifyFoundElement(By.id(PortalHtmlElements.MARKETPLACE_SPAN_WELCOME)));
+    assertTrue(tester.verifyFoundElement(By.id(MarketplaceHtmlElements.MARKETPLACE_SPAN_WELCOME)));
   }
 
   @Test
   public void test02addBillingAddress() throws InterruptedException {
 
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_BUTTON);
     tester.clickElementXPath(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_ADD_BILLING_ADDRESS_BUTTON);
-    tester.waitForElement(By.id(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD), 10);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_ADD_BILLING_ADDRESS_BUTTON);
+    tester.waitForElement(By.id(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD), 10);
     Thread.sleep(1000);
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD, "Smiths billing");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD, "Smiths billing");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD, "Smiths billing");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_DISPLAY_FIELD, "Smiths billing");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_NAME_FIELD, "Smiths organization");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_NAME_FIELD, "Smiths organization");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_EMAIL_FIELD, "smiths.organization@mail.com");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_EMAIL_FIELD, "smiths.organization@mail.com");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_ADDRESS_FIELD,
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_ADDRESS_FIELD,
         "Australia\nSmall Village 10");
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_SAVE_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_SAVE_BUTTON);
 
     assertTrue(tester.readInfoMessage().contains("The billing address has been successfully save"));
   }
 
   @Test
   public void test03addPaymentType() throws InterruptedException {
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_BUTTON);
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_MANAGE_PAYMENT_TABLE);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_MANAGE_PAYMENT_TABLE);
     Thread.sleep(1000);
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_PAYMENT_NAME_FIELD, "Smiths payment");
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_PAYMENT_SAVE_BUTTON);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_PAYMENT_NAME_FIELD, "Smiths payment");
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PAYMENT_PAYMENT_SAVE_BUTTON);
 
     assertTrue(
         tester.readInfoMessage().contains("The payment information has been successfully saved"));
@@ -100,17 +100,17 @@ public class MarketplaceAccountWT {
 
   @Test
   public void test04addUser() {
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_SHOW_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_ADD_USER_BUTTON);
-    tester.selectDropdown(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_TITLE_DROPDOWN, "MS");
-    tester.writeValue(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Isabella");
-    tester.writeValue(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_SHOW_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_ADD_USER_BUTTON);
+    tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_TITLE_DROPDOWN, "MS");
+    tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_FIRST_NAME_FIELD, "Isabella");
+    tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LAST_NAME_FIELD, "Smith");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "isabella.smith@mail.com");
-    tester.writeValue(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_USERID_FIELD, "isabelSmith");
-    tester.selectDropdown(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "de");
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_CREATE_BUTTON);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_EMAIL_FIELD, "isabella.smith@mail.com");
+    tester.writeValue(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_USERID_FIELD, "isabelSmith");
+    tester.selectDropdown(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROFILE_LANGUAGE_DROPDOWN, "de");
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_CREATE_BUTTON);
 
     assertTrue(
         tester
@@ -120,8 +120,8 @@ public class MarketplaceAccountWT {
 
   @Test
   public void test05addUserRoles() throws InterruptedException {
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_ADD_ROLE_CHECKBOX);
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_ROLE_ASSIGN_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_ADD_ROLE_CHECKBOX);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_ROLE_ASSIGN_BUTTON);
     Thread.sleep(1000);
     assertTrue(
         tester
@@ -131,18 +131,18 @@ public class MarketplaceAccountWT {
 
   @Test
   public void test06addOrganization() throws InterruptedException {
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_USERS_SHOW_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_UNIT_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_UNIT_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_USERS_SHOW_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_UNIT_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_UNIT_BUTTON);
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_NAME_FIELD, "Isabels organization");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_NAME_FIELD, "Isabels organization");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_ID_FIELD, "isabelorg");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_ID_FIELD, "isabelorg");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_DESCRIPTION_FIELD,
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ORG_DESCRIPTION_FIELD,
         "Isabel's family organization");
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_CREATE_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_CREATE_BUTTON);
     Thread.sleep(1000);
     assertTrue(
         tester
@@ -154,12 +154,12 @@ public class MarketplaceAccountWT {
   @Test
   public void test07addUsersToOrganization() throws InterruptedException {
     tester.clickElement(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_ADMINISTRATOR_CHECKBOX);
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_USER_CHECKBOX);
+            MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_ADMINISTRATOR_CHECKBOX);
+    tester.clickElement(    MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ADD_USER_CHECKBOX);
     tester.selectDropdown(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ASSIGN_ROLE_DROPDOWN, "ADMINISTRATOR");
+            MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ASSIGN_ROLE_DROPDOWN, "ADMINISTRATOR");
     tester.clickElementXPath(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ASSIGN_ROLE_BUTTON);
+            MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_ASSIGN_ROLE_BUTTON);
     Thread.sleep(1000);
     assertTrue(
         tester
@@ -169,10 +169,10 @@ public class MarketplaceAccountWT {
 
   @Test
   public void test08deleteOrganization() throws InterruptedException {
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_DELETE_INPUT);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_DELETE_INPUT);
     Thread.sleep(1000);
     tester.clickElementXPath(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_DELETE_CONFIRM_BUTTON);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_ORGANIZATION_DELETE_CONFIRM_BUTTON);
     Thread.sleep(1000);
     assertTrue(
         tester
@@ -183,20 +183,20 @@ public class MarketplaceAccountWT {
 
   @Test
   public void test09addProcessTrigger() throws InterruptedException {
-    tester.clickElement(PortalHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_SHOW_BUTTON);
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_ADD_PROCESS_BUTTON);
+    tester.clickElement(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_SHOW_BUTTON);
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_ADD_PROCESS_BUTTON);
     Thread.sleep(1000);
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TRIGGER_NAME_FIELD, "Trigger");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TRIGGER_NAME_FIELD, "Trigger");
     tester.selectDropdown(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TRIGGER_TYPE_DROPDOWN,
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TRIGGER_TYPE_DROPDOWN,
         "SUBSCRIBE_TO_SERVICE");
     tester.selectDropdown(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_TYPE_DROPDOWN, "REST_SERVICE");
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_TYPE_DROPDOWN, "REST_SERVICE");
     tester.writeValue(
-        PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_URL_FIELD, "https://webiste.io");
-    tester.clickElementXPath(PortalHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_SAVE_BUTTON);
+        MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_URL_FIELD, "https://webiste.io");
+    tester.clickElementXPath(MarketplaceHtmlElements.MARKETPLACE_ACCOUNT_PROCESSES_TARGET_SAVE_BUTTON);
 
     assertTrue(
         tester.readInfoMessage().contains("The trigger definition has been successfully created."));
