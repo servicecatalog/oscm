ddc4163fe4cacba8070d0d8d47bf117a53bb0677
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2020-06-08 10:32:40 +0200

    Resolved broken encoding in header

--- oscm-security-unittests/javasrc/org/oscm/security/ADMRealmImplTest.java
+++ oscm-security-unittests/javasrc/org/oscm/security/ADMRealmImplTest.java
@@ -2,7 +2,7 @@
  *                                                                              
  *  Copyright FUJITSU LIMITED 2018
  *                                                                                                                                 
- *  Creation Date: 2015ĺą´3ćś�30ć—Ą                                                      
+ *  Creation Date: 2015-03-30                                                   
  *                                                                              
  *******************************************************************************/
 
@@ -15,7 +15,6 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
