863d5a9b6a08178609e6ab373be25d8a7aa95367
cworf91 <christian.worf@est.fujitsu.com>
2020-07-06 11:26:01 +0200

    fix logout test

--- oscm-portal/javasrc/org/oscm/ui/filter/OidcLogoutFilter.java
+++ oscm-portal/javasrc/org/oscm/ui/filter/OidcLogoutFilter.java
@@ -95,16 +95,19 @@ public class OidcLogoutFilter extends BaseBesFilter {
 
     String requestedUrl = request.getRequestURL().toString();
 
-    String isu =
-        getConfigurationService(request)
-            .getVOConfigurationSetting(
-                ConfigurationKey.OSCM_IDENTITY_SERVICE_URL, Configuration.GLOBAL_CONTEXT)
-            .getValue();
+    String isu = getConfigKeyValue(request);
 
     URI uri = new URI(requestedUrl);
     String redirectionUrl = buildRedirectionUrl(uri, path);
     String logoutUrl = isu + "/logout?state=" + redirectionUrl;
     return logoutUrl;
+  }
+
+  protected String getConfigKeyValue(HttpServletRequest request) {
+    return getConfigurationService(request)
+        .getVOConfigurationSetting(
+            ConfigurationKey.OSCM_IDENTITY_SERVICE_URL, Configuration.GLOBAL_CONTEXT)
+        .getValue();
   }
 
   private boolean isLogoutRequested(HttpServletRequest request) {
