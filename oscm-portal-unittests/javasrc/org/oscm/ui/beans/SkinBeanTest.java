6044e29794d3810f73b6edf23defc465b2477ffb
farmakiG <49904723+farmakiG@users.noreply.github.com>
2020-07-10 16:50:46 +0200

    Fix responsive stage (#1014)
    * Enclose iframe for branded stage inside a div for responsive container.
    
    * Review comments.
    
    * Corrected failing Unit Tests.
    
    * renamed resultDiv to expectedDiv.

--- oscm-portal-unittests/javasrc/org/oscm/ui/beans/SkinBeanTest.java
+++ oscm-portal-unittests/javasrc/org/oscm/ui/beans/SkinBeanTest.java
@@ -1,15 +1,16 @@
-/*******************************************************************************
- *                                                                              
- *  Copyright FUJITSU LIMITED 2018
- *                                                                              
- *  Author: Florian Walker                                                   
- *                                                                              
- *  Creation Date: 28.04.2011                                                      
- *                                                                              
- *  Completion Time: 28.04.2011                                                  
- *                                                                              
- *******************************************************************************/
-
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2018
+ *
+ * <p>Author: Florian Walker
+ *
+ * <p>Creation Date: 28.04.2011
+ *
+ * <p>Completion Time: 28.04.2011
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.ui.beans;
 
 import static org.mockito.Matchers.anyString;
@@ -29,130 +30,129 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.junit.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
-
-import org.oscm.ui.common.Constants;
 import org.oscm.internal.intf.BrandService;
 import org.oscm.internal.vo.VOUserDetails;
+import org.oscm.ui.common.Constants;
 
 /**
  * Tests the skin bean.
- * 
+ *
  * @author Florian Walker
  */
 public class SkinBeanTest {
 
-    private final String stageContent = "STAGE_CONTENT_DEFAULT";
+  private final String stageContent = "STAGE_CONTENT_DEFAULT";
+  private final String stageContentBranded = "STAGE_CONTENT_BRANDED";
 
-    private SkinBean skinBean;
+  private SkinBean skinBean;
 
-    private BrandService brandServiceMock;
-    private HttpSession httpSessionMock;
-    private UIViewRoot viewRootMock;
+  private BrandService brandServiceMock;
+  private HttpSession httpSessionMock;
+  private UIViewRoot viewRootMock;
 
-    @Before
-    public void before() {
-        // mock the BrandService' get getMarketplaceStage() method
-        brandServiceMock = mock(BrandService.class);
-        when(brandServiceMock.getMarketplaceStage(anyString(), anyString()))
-                .thenReturn(stageContent);
+  @Before
+  public void before() {
+    // mock the BrandService' get getMarketplaceStage() method
+    brandServiceMock = mock(BrandService.class);
+    when(brandServiceMock.getMarketplaceStage(anyString(), anyString())).thenReturn(stageContent);
 
-        // mock the HttpSession's getAttribute() method
-        httpSessionMock = mock(HttpSession.class);
-        when(httpSessionMock.getAttribute(anyString())).thenReturn(null);
+    // mock the HttpSession's getAttribute() method
+    httpSessionMock = mock(HttpSession.class);
+    when(httpSessionMock.getAttribute(anyString())).thenReturn(null);
 
-        // mock the HttpServletRequest's getSession() method
-        HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
-        when(httpServletRequestMock.getSession()).thenReturn(httpSessionMock);
+    // mock the HttpServletRequest's getSession() method
+    HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
+    when(httpServletRequestMock.getSession()).thenReturn(httpSessionMock);
 
-        // mock the ExternalContext's getRequest() method
-        ExternalContext externalContextMock = mock(ExternalContext.class);
-        when(externalContextMock.getRequest()).thenReturn(
-                httpServletRequestMock);
+    // mock the ExternalContext's getRequest() method
+    ExternalContext externalContextMock = mock(ExternalContext.class);
+    when(externalContextMock.getRequest()).thenReturn(httpServletRequestMock);
 
-        // mock the UIViewRoot's getLocale() method
-        viewRootMock = mock(UIViewRoot.class);
-        when(viewRootMock.getLocale()).thenReturn(Locale.FRANCE);
+    // mock the UIViewRoot's getLocale() method
+    viewRootMock = mock(UIViewRoot.class);
+    when(viewRootMock.getLocale()).thenReturn(Locale.FRANCE);
 
-        // mock the FacesContext's getExternalContext() and getViewRoot()
-        // methods
-        final FacesContext facesContextMock = mock(FacesContext.class);
-        when(facesContextMock.getExternalContext()).thenReturn(
-                externalContextMock);
-        when(facesContextMock.getViewRoot()).thenReturn(viewRootMock);
+    // mock the FacesContext's getExternalContext() and getViewRoot()
+    // methods
+    final FacesContext facesContextMock = mock(FacesContext.class);
+    when(facesContextMock.getExternalContext()).thenReturn(externalContextMock);
+    when(facesContextMock.getViewRoot()).thenReturn(viewRootMock);
 
-        // spy the SkinBean to only partial mock - overriding the methods below
-        skinBean = spy(new SkinBean());
-        doReturn(brandServiceMock).when(skinBean).getBrandManagementService();
-        doReturn(facesContextMock).when(skinBean).getFacesContext();
-        doReturn(httpServletRequestMock).when(skinBean).getRequest();
-    }
+    // spy the SkinBean to only partial mock - overriding the methods below
+    skinBean = spy(new SkinBean());
+    doReturn(brandServiceMock).when(skinBean).getBrandManagementService();
+    doReturn(facesContextMock).when(skinBean).getFacesContext();
+    doReturn(httpServletRequestMock).when(skinBean).getRequest();
+  }
 
-    /**
-     * Anonymous case => browser locale.
-     */
-    @Test
-    public void testGetMarketPlaceStage_browseLocale() {
-        when(viewRootMock.getLocale()).thenReturn(Locale.ENGLISH);
-        when(
-                brandServiceMock.getMarketplaceStage(anyString(),
-                        eq(Locale.ENGLISH.getLanguage()))).thenReturn(
-                stageContent);
-        Assert.assertEquals(stageContent, skinBean.getMarketplaceStage());
-    }
+  /** Anonymous case => browser locale. */
+  @Test
+  public void testGetMarketPlaceStage_browseLocale() {
+    when(viewRootMock.getLocale()).thenReturn(Locale.ENGLISH);
+    when(brandServiceMock.getMarketplaceStage(anyString(), eq(Locale.ENGLISH.getLanguage())))
+        .thenReturn(stageContent);
 
-    /**
-     * vouser in session.
-     */
-    @Test
-    public void testGetMarketPlaceStage_userLocaleSessionOK() {
-        VOUserDetails voUser = new VOUserDetails();
-        voUser.setLocale(Locale.CHINA.getLanguage());
-        when(httpSessionMock.getAttribute(eq(Constants.SESS_ATTR_USER)))
-                .thenReturn(voUser);
-        when(
-                brandServiceMock.getMarketplaceStage(anyString(),
-                        eq(Locale.CHINA.getLanguage()))).thenReturn(
-                stageContent);
-        Assert.assertEquals(stageContent, skinBean.getMarketplaceStage());
-    }
+    String expectedDiv = MessageFormat.format(SkinBean.MARKETPLACE_STAGE_BRANDED, stageContent);
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+  }
 
-    /**
-     * session available but no vo => browser locale
-     */
-    @Test
-    public void testGetMarketPlaceStage_userLocaleSessionNoVo() {
-        when(
-                brandServiceMock.getMarketplaceStage(anyString(),
-                        eq(Locale.FRANCE.getLanguage()))).thenReturn(
-                stageContent);
-        Assert.assertEquals(stageContent, skinBean.getMarketplaceStage());
-    }
+  /** vouser in session. */
+  @Test
+  public void testGetMarketPlaceStage_userLocaleSessionOK() {
+    // given
+    VOUserDetails voUser = new VOUserDetails();
+    voUser.setLocale(Locale.CHINA.getLanguage());
+    when(httpSessionMock.getAttribute(eq(Constants.SESS_ATTR_USER))).thenReturn(voUser);
 
-    @Test
-    public void testGetMarketPlaceStage_default() {
-        when(
-                brandServiceMock.getMarketplaceStage(anyString(),
-                        eq(Locale.FRANCE.getLanguage()))).thenReturn("");
-        when(skinBean.getRequestContextPath()).thenReturn("");
-        Assert.assertEquals(
-                MessageFormat.format(SkinBean.MARKETPLACE_STAGE_DEFAULT, ""),
-                skinBean.getMarketplaceStage());
-    }
+    // when
+    when(brandServiceMock.getMarketplaceStage(anyString(), eq(Locale.CHINA.getLanguage())))
+        .thenReturn(stageContentBranded);
+    String expectedDiv =
+        MessageFormat.format(SkinBean.MARKETPLACE_STAGE_BRANDED, stageContentBranded);
 
-    /**
-     * Caching test (just for the coverage)
-     */
-    @Test
-    public void testGetMarketPlaceStage_cache() {
-        when(viewRootMock.getLocale()).thenReturn(Locale.ENGLISH);
-        when(
-                brandServiceMock.getMarketplaceStage(anyString(),
-                        eq(Locale.ENGLISH.getLanguage()))).thenReturn(
-                stageContent);
-        Assert.assertEquals(stageContent, skinBean.getMarketplaceStage());
-        Assert.assertEquals(stageContent, skinBean.getMarketplaceStage());
-    }
+    // then
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+  }
+
+  /** session available but no vo => browser locale */
+  @Test
+  public void testGetMarketPlaceStage_userLocaleSessionNoVo() {
+    // given
+    when(brandServiceMock.getMarketplaceStage(anyString(), eq(Locale.FRANCE.getLanguage())))
+        .thenReturn(stageContent);
+
+    // then
+    String expectedDiv = MessageFormat.format(SkinBean.MARKETPLACE_STAGE_BRANDED, stageContent);
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+  }
+
+  @Test
+  public void testGetMarketPlaceStage_default() {
+    when(brandServiceMock.getMarketplaceStage(anyString(), eq(Locale.FRANCE.getLanguage())))
+        .thenReturn("");
+    when(skinBean.getRequestContextPath()).thenReturn("");
+    Assert.assertEquals(
+        MessageFormat.format(SkinBean.MARKETPLACE_STAGE_DEFAULT, ""),
+        skinBean.getMarketplaceStage());
+  }
+
+  /** Caching test (just for the coverage) */
+  @Test
+  public void testGetMarketPlaceStage_cache() {
+    // given
+    when(viewRootMock.getLocale()).thenReturn(Locale.ENGLISH);
+    when(brandServiceMock.getMarketplaceStage(anyString(), eq(Locale.ENGLISH.getLanguage())))
+        .thenReturn(stageContentBranded);
+
+    // then
+    String expectedDiv =
+        MessageFormat.format(SkinBean.MARKETPLACE_STAGE_BRANDED, stageContentBranded);
+
+    // then
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+    Assert.assertEquals(expectedDiv, skinBean.getMarketplaceStage());
+  }
 }
