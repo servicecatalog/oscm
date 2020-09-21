bf4f1a9a8278e72da0db7c7f2039bd68bf4fed84
kabaciski <piotr.kabacinski@ts.fujitsu.com>
2020-09-21 12:13:08 +0200

    Add information about provisioning url

--- oscm-portal/javasrc/org/oscm/ui/beans/ServiceBean.java
+++ oscm-portal/javasrc/org/oscm/ui/beans/ServiceBean.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Locale;
 
+import javax.faces.FacesException;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -35,15 +36,8 @@ import org.oscm.internal.types.enumtypes.ParameterValueType;
 import org.oscm.internal.types.enumtypes.PerformanceHint;
 import org.oscm.internal.types.enumtypes.PriceModelType;
 import org.oscm.internal.types.enumtypes.ServiceStatus;
-import org.oscm.internal.types.exception.ConcurrentModificationException;
+import org.oscm.internal.types.exception.*;
 import org.oscm.internal.types.exception.DomainObjectException.ClassEnum;
-import org.oscm.internal.types.exception.ImageException;
-import org.oscm.internal.types.exception.ObjectNotFoundException;
-import org.oscm.internal.types.exception.OperationNotPermittedException;
-import org.oscm.internal.types.exception.OrganizationAuthoritiesException;
-import org.oscm.internal.types.exception.PublishingToMarketplaceNotPermittedException;
-import org.oscm.internal.types.exception.SaaSApplicationException;
-import org.oscm.internal.types.exception.ServiceStateException;
 import org.oscm.internal.vo.VOCatalogEntry;
 import org.oscm.internal.vo.VOCategory;
 import org.oscm.internal.vo.VOCompatibleService;
@@ -59,6 +53,7 @@ import org.oscm.internal.vo.VOServiceLocalization;
 import org.oscm.internal.vo.VOTechnicalService;
 import org.oscm.logging.Log4jLogger;
 import org.oscm.logging.LoggerFactory;
+import org.oscm.types.exceptions.ConnectException;
 import org.oscm.ui.common.ExceptionHandler;
 import org.oscm.ui.common.ImageUploader;
 import org.oscm.ui.common.JSFUtils;
@@ -86,6 +81,7 @@ public class ServiceBean extends BaseBean implements Serializable {
 
     private static final String SERVICE_NO_LONGER_EXISTS = "error.service.noLonger.exists";
     private static final String SERVICE_DELETED = "error.service.deleted";
+    private static final String SERVICE_NOT_ALIVE = "error.service.notAlive";
     private static final String CANNOT_DELETE_ACTIVE_SERVICE = "error.service.cannot.delete.active";
 
     private List<VOTechnicalService> availableTechServices;
@@ -978,6 +974,9 @@ public class ServiceBean extends BaseBean implements Serializable {
             } else
                 throw e;
 
+        } catch (RuntimeException e) {
+            addMessage(null, FacesMessage.SEVERITY_ERROR,
+                    SERVICE_NOT_ALIVE);
         } finally {
             // clear caches (enforce reread next time)
             servicesForDeActivation = null;
