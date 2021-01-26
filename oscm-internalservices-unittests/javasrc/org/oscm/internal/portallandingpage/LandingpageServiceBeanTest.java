3216f269147e56bc6ca5b5bd9072a200ee0b86f3
KabacinskiPiotr <piotr.kabacinski@ts.fujitsu.com>
2021-01-26 10:18:18 +0100

    Add test with case if the service will be PARTNER_TEMPLATE type

--- oscm-internalservices-unittests/javasrc/org/oscm/internal/portallandingpage/LandingpageServiceBeanTest.java
+++ oscm-internalservices-unittests/javasrc/org/oscm/internal/portallandingpage/LandingpageServiceBeanTest.java
@@ -9,36 +9,32 @@
  */
 package org.oscm.internal.portallandingpage;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.oscm.dataservice.local.DataService;
-import org.oscm.domobjects.DomainObject;
 import org.oscm.domobjects.ImageResource;
 import org.oscm.domobjects.Product;
 import org.oscm.i18nservice.local.ImageResourceServiceLocal;
 import org.oscm.internal.types.enumtypes.ImageType;
 import org.oscm.internal.types.enumtypes.ServiceType;
-import org.oscm.internal.types.exception.ObjectNotFoundException;
 import org.oscm.internal.vo.VOImageResource;
 import org.oscm.internal.vo.VOService;
 
-/** @author worf */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
 public class LandingpageServiceBeanTest {
 
   private DataService ds;
   private ImageResourceServiceLocal irsl;
   private LandingpageServiceBean bean;
+  private Product product;
+  private Product productPartner;
 
   @Before
   public void setUp() {
@@ -50,7 +46,7 @@ public class LandingpageServiceBeanTest {
   }
 
   @Test
-  public void fillInServiceImages() throws ObjectNotFoundException {
+  public void fillInServiceImages() {
 
     // given
     List<VOService> services = new ArrayList<>();
@@ -59,19 +55,54 @@ public class LandingpageServiceBeanTest {
     service.setKey(123456789);
     service.setServiceType(ServiceType.TEMPLATE);
     services.add(service);
-    byte[] bytes = new byte[] {11, 22, 33, 44};
+    byte[] bytes = new byte[]{11, 22, 33, 44};
 
     ImageResource imageResource = new ImageResource(123456789, ImageType.SERVICE_IMAGE);
     imageResource.setBuffer(bytes);
 
-    Product product = new Product();
-    product.setType(ServiceType.PARTNER_TEMPLATE);
-    product.setProductId("partner");
-    product.setKey(10000000);
-    product.setTemplate(ds.getReference(Product.class, service.getKey()));
+    product = new Product();
+    product.setKey(987654321);
+    product.setProductId("product");
+    product.setType(ServiceType.TEMPLATE);
 
     doReturn(product).when(ds).find(any(), anyLong());
-    doReturn(imageResource).when(irsl).read(eq(123456789), anyObject());
+    doReturn(imageResource).when(irsl).read(anyLong(), anyObject());
+
+    // when
+    Map<Long, VOImageResource> result = bean.fillInServiceImages(services);
+
+    // then
+    assertEquals(ImageType.SERVICE_IMAGE, result.get(service.getKey()).getImageType());
+    assertEquals(bytes, result.get(service.getKey()).getBuffer());
+  }
+
+  @Test
+  public void fillInServiceImagesPartnerTemplate() {
+
+    // given
+    List<VOService> services = new ArrayList<>();
+    VOService service = new VOService();
+    service.setServiceId("test");
+    service.setKey(123456789);
+    service.setServiceType(ServiceType.TEMPLATE);
+    services.add(service);
+    byte[] bytes = new byte[]{11, 22, 33, 44};
+
+    ImageResource imageResource = new ImageResource(123456789, ImageType.SERVICE_IMAGE);
+    imageResource.setBuffer(bytes);
+
+    product = new Product();
+    product.setType(ServiceType.TEMPLATE);
+    product.setProductId("template");
+    product.setKey(987654321);
+    productPartner = new Product();
+    productPartner.setType(ServiceType.PARTNER_TEMPLATE);
+    productPartner.setProductId("partner");
+    productPartner.setKey(10000000);
+    productPartner.setTemplate(product);
+
+    doReturn(productPartner).when(ds).find(any(), anyLong());
+    doReturn(imageResource).when(irsl).read(eq(987654321L), anyObject());
 
     // when
     Map<Long, VOImageResource> result = bean.fillInServiceImages(services);
