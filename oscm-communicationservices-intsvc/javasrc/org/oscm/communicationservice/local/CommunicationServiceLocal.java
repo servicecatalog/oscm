6c2bbd94720d0ecefa9f8d377d36cfd6a7030114
Lorenz Goebel <GoebelL@users.noreply.github.com>
2021-04-26 14:29:11 +0200

    Allow mail mock to retrieve initial user emails (#1241)

--- oscm-communicationservices-intsvc/javasrc/org/oscm/communicationservice/local/CommunicationServiceLocal.java
+++ oscm-communicationservices-intsvc/javasrc/org/oscm/communicationservice/local/CommunicationServiceLocal.java
@@ -1,7 +1,7 @@
-/*******************************************************************************
- *  Copyright FUJITSU LIMITED 2018
- *******************************************************************************/
-
+/**
+ * ***************************************************************************** Copyright FUJITSU
+ * LIMITED 2018 *****************************************************************************
+ */
 package org.oscm.communicationservice.local;
 
 import javax.ejb.Local;
@@ -16,171 +16,154 @@ import org.oscm.internal.types.exception.ValidationException;
 
 /**
  * Service interface providing functionality to send e-mails.
- * 
- * <p>
- * The service uses the configuration service to obtain the mail server address
- * and the reply mail address.
- * </p>
- * 
+ *
+ * <p>The service uses the configuration service to obtain the mail server address and the reply
+ * mail address.
+ *
  * @author Mike J&auml;ger
- * 
  */
 @Local
 public interface CommunicationServiceLocal {
 
-    /**
-     * Sends an email to the given platform users.
-     * 
-     * @param type
-     *            The email content type.
-     * @param params
-     *            Parameters for the email content text.
-     * @param marketplace
-     *            the marketplace to get customized texts from - can be
-     *            <code>null</code>
-     * @param recipients
-     *            The platform users the mail will be sent to.
-     * @return mail status of every sended mail encapsulated in a separate
-     *         instance
-     */
-    public SendMailStatus<PlatformUser> sendMail(EmailType type,
-            Object[] params, Marketplace marketplace,
-            PlatformUser... recipients);
+  /**
+   * Sends an email to the given platform users.
+   *
+   * @param type The email content type.
+   * @param params Parameters for the email content text.
+   * @param marketplace the marketplace to get customized texts from - can be <code>null</code>
+   * @param recipients The platform users the mail will be sent to.
+   * @return mail status of every sended mail encapsulated in a separate instance
+   */
+  public SendMailStatus<PlatformUser> sendMail(
+      EmailType type, Object[] params, Marketplace marketplace, PlatformUser... recipients);
 
-    /**
-     * Sends an email to the given platform user.
-     * 
-     * @param recipient
-     *            The platform user the mail will be sent to.
-     * @param type
-     *            The email content type.
-     * @param params
-     *            Parameters for the email content text.
-     * @param marketplace
-     *            the marketplace to get customized texts from - can be
-     *            <code>null</code>
-     * @throws MailOperationException
-     *             Thrown in case the mail cannot be initialized or sent.
-     */
-    public void sendMail(PlatformUser recipient, EmailType type,
-            Object[] params, Marketplace marketplace)
-            throws MailOperationException;
+  /**
+   * Sends an email to the given platform user.
+   *
+   * @param recipient The platform user the mail will be sent to.
+   * @param type The email content type.
+   * @param params Parameters for the email content text.
+   * @param marketplace the marketplace to get customized texts from - can be <code>null</code>
+   * @throws MailOperationException Thrown in case the mail cannot be initialized or sent.
+   */
+  public void sendMail(
+      PlatformUser recipient, EmailType type, Object[] params, Marketplace marketplace)
+      throws MailOperationException;
 
-    /**
-     * Send mail to organization.
-     * 
-     * @param organization
-     *            Organization to getting mail.
-     * @param type
-     *            Mail type.
-     * @param params
-     *            Mail parameters,
-     * @param marketplace
-     *            The marketplace to get customized texts from - can be
-     *            <code>null</code>
-     * @throws MailOperationException
-     *             On error mail sending.
-     */
-    public void sendMail(Organization organization, EmailType type,
-            Object[] params, Marketplace marketplace)
-            throws MailOperationException;
+  /**
+   * Send mail to organization.
+   *
+   * @param organization Organization to getting mail.
+   * @param type Mail type.
+   * @param params Mail parameters,
+   * @param marketplace The marketplace to get customized texts from - can be <code>null</code>
+   * @throws MailOperationException On error mail sending.
+   */
+  public void sendMail(
+      Organization organization, EmailType type, Object[] params, Marketplace marketplace)
+      throws MailOperationException;
 
-    /**
-     * Send an email of given type to the given address.
-     * 
-     * @param emailAddress
-     *            The address to which the mail will be send to.
-     * @param type
-     *            Mail type.
-     * @param params
-     *            Mail parameters,
-     * @param marketplace
-     *            Marketpalce of subscription,
-     * @param locale
-     *            Locale information of mail receiver,
-     * @throws MailOperationException
-     *             On error mail sending.
-     * @throws ValidationException
-     *             if the format of the email address is not valid
-     */
-    public void sendMail(String emailAddress, EmailType type, Object[] params,
-            Marketplace marketplace, String locale)
-            throws MailOperationException, ValidationException;
+  /**
+   * Send an email of given type to the given address.
+   *
+   * @param emailAddress The address to which the mail will be send to.
+   * @param type Mail type.
+   * @param params Mail parameters,
+   * @param marketplace Marketpalce of subscription,
+   * @param locale Locale information of mail receiver,
+   * @throws MailOperationException On error mail sending.
+   * @throws ValidationException if the format of the email address is not valid
+   */
+  public void sendMail(
+      String emailAddress, EmailType type, Object[] params, Marketplace marketplace, String locale)
+      throws MailOperationException, ValidationException;
 
-    /**
-     * Send mail to given organizations.
-     * 
-     * @param type
-     *            Mail type.
-     * @param params
-     *            Mail parameters,
-     * @param marketplace
-     *            The marketplace to get customized texts from - can be
-     *            <code>null</code>
-     * @param organizations
-     *            The organizations the mail will be sent to.
-     * @return mail status of every sended mail encapsulated in a separate
-     *         instance
-     */
-    public SendMailStatus<Organization> sendMail(EmailType type,
-            Object[] params, Marketplace marketplace,
-            Organization... organizations);
+  /**
+   * Send mail to given organizations.
+   *
+   * @param type Mail type.
+   * @param params Mail parameters,
+   * @param marketplace The marketplace to get customized texts from - can be <code>null</code>
+   * @param organizations The organizations the mail will be sent to.
+   * @return mail status of every sended mail encapsulated in a separate instance
+   */
+  public SendMailStatus<Organization> sendMail(
+      EmailType type, Object[] params, Marketplace marketplace, Organization... organizations);
 
-    /**
-     * Get the marketplace URL for the given platform user.
-     * 
-     * @param marketplaceId
-     *            the marketplace id
-     * @return The marketplace URL for the given platform user.
-     * @throws MailOperationException
-     *             Thrown if an UnsupportedEncodingException occurs during the
-     *             URL construction.
-     */
-    public String getMarketplaceUrl(String marketplaceId)
-            throws MailOperationException;
+  /**
+   * Get the marketplace URL for the given platform user.
+   *
+   * @param marketplaceId the marketplace id
+   * @return The marketplace URL for the given platform user.
+   * @throws MailOperationException Thrown if an UnsupportedEncodingException occurs during the URL
+   *     construction.
+   */
+  public String getMarketplaceUrl(String marketplaceId) throws MailOperationException;
 
-    /**
-     * Get the marketplace URL for the given platform user.
-     *
-     * @param marketplaceId
-     *            the marketplace id
-     * @return The secure marketplace URL (https) for the given platform user.
-     * @throws MailOperationException
-     *             Thrown if an UnsupportedEncodingException occurs during the
-     *             URL construction.
-     */
-    public String getMarketplaceUrlHttps(String marketplaceId)
-            throws MailOperationException;
+  /**
+   * Get the marketplace URL for the given platform user.
+   *
+   * @param marketplaceId the marketplace id
+   * @return The secure marketplace URL (https) for the given platform user.
+   * @throws MailOperationException Thrown if an UnsupportedEncodingException occurs during the URL
+   *     construction.
+   */
+  public String getMarketplaceUrlHttps(String marketplaceId) throws MailOperationException;
 
-    /**
-     * Gets the administration URL of the blue portal
-     * 
-     * @return the administration URL of the blue portal
-     */
-    public String getBaseUrl();
+  /**
+   * Gets the administration URL of the blue portal
+   *
+   * @return the administration URL of the blue portal
+   */
+  public String getBaseUrl();
 
-    /**
-     * Gets the secure administration URL (https) of the blue portal
-     *
-     * @return the secure administration URL (https) of the blue portal
-     */
-    public String getBaseUrlHttps();
+  /**
+   * Gets the secure administration URL (https) of the blue portal
+   *
+   * @return the secure administration URL (https) of the blue portal
+   */
+  public String getBaseUrlHttps();
 
-    /**
-     * Gets the administration URL of the blue portal with ID of the tenant
-     *
-     * @param tenantId
-     *            the tenant ID
-     * @return
-     */
-    public String getBaseUrlWithTenant(String tenantId) throws MailOperationException;
+  /**
+   * Gets the administration URL of the blue portal with ID of the tenant
+   *
+   * @param tenantId the tenant ID
+   * @return
+   */
+  public String getBaseUrlWithTenant(String tenantId) throws MailOperationException;
 
-    /**
-     * Gets the administration secure URL of the blue portal with ID of the tenant
-     *
-     * @param tenantId
-     *            the tenant ID
-     * @return
-     */
-    public String getBaseUrlHttpsWithTenant(String tenantId) throws MailOperationException;
+  /**
+   * Gets the administration secure URL of the blue portal with ID of the tenant
+   *
+   * @param tenantId the tenant ID
+   * @return
+   */
+  public String getBaseUrlHttpsWithTenant(String tenantId) throws MailOperationException;
+
+  /**
+   * Sends an email to the given address for given target user.
+   *
+   * @param emailAddress where the mail will be sent to.
+   * @param targetUser the target user addressed in this email
+   * @param type The email content type.
+   * @param params Parameters for the email content text.
+   * @param marketplace the marketplace to get customized texts from - can be <code>null</code>
+   * @throws MailOperationException Thrown in case the mail cannot be initialized or sent.
+   */
+  public void sendMail(
+      String emailAddress,
+      PlatformUser targetUser,
+      EmailType type,
+      Object[] params,
+      Marketplace marketplace)
+      throws MailOperationException;
+
+  /**
+   * if the OSCM mail mock service is used.
+   *
+   * <p>
+   *
+   * @return <code>true</code> if the mail mock service is used, otherwise <code>false</code>.
+   */
+  public boolean isMailMock();
 }
--- oscm-communicationservices/javasrc/org/oscm/communicationservice/Constants.java
+++ oscm-communicationservices/javasrc/org/oscm/communicationservice/Constants.java
@@ -1,37 +1,33 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  *
- *  Copyright FUJITSU LIMITED 2018
+ * <p>Copyright FUJITSU LIMITED 2018
  *
- *  Creation Date: 21.09.16 08:07
+ * <p>Creation Date: 21.09.16 08:07
  *
- ******************************************************************************/
-
+ * <p>****************************************************************************
+ */
 package org.oscm.communicationservice;
 
-/**
- * Class which holds constants values for the communicationservices module.
- * Authored by dawidch
- */
+/** Class which holds constants values for the communicationservices module. Authored by dawidch */
 public class Constants {
-    public static final String TENANT_ID = "tenantID";
-    public static final String ENCODING = "UTF-8";
-    public static final String MAIL_PROTOCOL_SMTP = "smtp";
+  public static final String TENANT_ID = "tenantID";
+  public static final String ENCODING = "UTF-8";
+  public static final String MAIL_PROTOCOL_SMTP = "smtp";
 
-    public static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
-    public static final String MAIL_PASSWORD = "mail.smtp.password";
-    public static final String MAIL_USER = "mail.user";
-    public static final String MAIL_SMTP_USER = "mail.smtp.user";
-    public static final String MAIL_TLS_ENABLED = "mail.smtp.starttls.enable";
+  public static final String MAIL_SMTP_HOST = "mail.smtp.host";
+  public static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+  public static final String MAIL_PASSWORD = "mail.smtp.password";
+  public static final String MAIL_USER = "mail.user";
+  public static final String MAIL_SMTP_USER = "mail.smtp.user";
+  public static final String MAIL_TLS_ENABLED = "mail.smtp.starttls.enable";
 
+  public static final String RESOURCE_TEXT_HEADER = "text.header";
+  public static final String RESOURCE_TEXT_FOOTER = "text.footer";
+  public static final String RESOURCE_SUBJECT = ".subject";
+  public static final String RESOURCE_TEXT = ".text";
 
-    public static final String RESOURCE_TEXT_HEADER = "text.header";
-    public static final String RESOURCE_TEXT_FOOTER = "text.footer";
-    public static final String RESOURCE_SUBJECT = ".subject";
-    public static final String RESOURCE_TEXT = ".text";
+  public static final String MAIL_RESOURCE = "java:openejb/Resource/OSCMMail";
 
-    public static final String MAIL_RESOURCE = "java:openejb/Resource/OSCMMail";
-
-
-    private Constants() {
-    }
+  private Constants() {}
 }
